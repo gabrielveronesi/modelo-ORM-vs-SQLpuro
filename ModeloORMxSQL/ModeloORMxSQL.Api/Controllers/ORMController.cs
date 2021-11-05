0f67e524bdf03ae3b5d5763e4f620bc89ae32275
Gabriel Veronesi <57780382+gabrielveronesi@users.noreply.github.com>
2021-11-05 16:21:27 -0300

    Update ORMController.cs

--- ModeloORMxSQL/ModeloORMxSQL.Api/Controllers/ORMController.cs
+++ ModeloORMxSQL/ModeloORMxSQL.Api/Controllers/ORMController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ModeloORMxSQL.Core.Models;
@@ -29,8 +31,28 @@ namespace ModeloORMxSQL.Api.Controllers
             {
                 return Resposta(false, "Não foi localizado clientes");
             }
+            
 
             return Resposta(true, new { listarClientes });
+        }
+
+        /// <summary>
+        /// Realizar agendamento passando um cliente e horario
+        /// </summary>
+        /// <param name="Id">Identificador do cliente que vai agendar</param>
+        /// <param name="DataAgendamento">Horário do agendamento 2021-03-25 00:00:00</param>
+        [HttpPost]
+        [Route("agendamento-orm")]
+        public ActionResult AgendamentoORM([Required] int Id, 
+                                           [Required] DateTime DataAgendamento)
+        {
+            var agendamentoCalculado = DataAgendamento.AddMinutes(40);
+
+            return Resposta(true, new {
+                Cliente       = "Gabriel",
+                HorarioInicio = DataAgendamento,
+                HorarioFim    = agendamentoCalculado,
+                });;
         }
     }
 }
