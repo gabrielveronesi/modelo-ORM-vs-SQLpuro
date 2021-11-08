6375e536db4cf2ba9d05c4b55c43901a5e654ff5
Gabriel Veronesi <57780382+gabrielveronesi@users.noreply.github.com>
2021-11-08 10:52:40 -0300

    //teste2

--- ModeloORMxSQL/ModeloORMxSQL.Api/Controllers/ORMController.cs
+++ ModeloORMxSQL/ModeloORMxSQL.Api/Controllers/ORMController.cs
@@ -33,7 +33,7 @@ namespace ModeloORMxSQL.Api.Controllers
             }
             
             //teste1
-            
+            //teste2
 
             return Resposta(true, new { listarClientes });
         }
